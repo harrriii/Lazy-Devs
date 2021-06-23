aa056ea6f530af72849f5a956a8c3e8b84d244fb
harrriii <kdlanguido@gmail.com>
2021-06-23 12:22:30 +0800

    first commit

---
+++ resources/views/inc/func/footer.blade.php
@@ -0,0 +1,18 @@
+<script src="{{asset('js/include/jquery-3.4.1.min.js')}}" ></script>
+<script src="{{asset('js/include/popper.min.js')}}" ></script>
+<script src="{{asset('js/include/bootstrap.min.js')}}" ></script>
+<script src="{{asset('js/include/feather.min.js')}}" ></script>
+<script src="{{asset('js/include/daterangepicker.min.js')}}" ></script>
+<script src="{{asset('js/include/aes.js')}}" ></script>
+<script src="{{asset('js/include/Nibbler.js')}}" ></script>
+<script src="{{asset('LD/func/LazyDevz.js')}}" ></script>
+
+
+
+
+
+
+
+<script>
+  feather.replace()
+</script> 
